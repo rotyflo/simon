bab626e3c2f5f39852ddae863eba095dddcb15ff
RT Flores <rotyflo@gmail.com>
2017-12-05 11:58:47 +0900

    Clean up code

--- js/main.js
+++ js/main.js
@@ -1,48 +1,32 @@
 "use strict";
 
-const GAME = {
-  colors: ["red", "blue", "yellow", "green"],
-  onOpacity: "1",
-  offOpacity: "0.8",
-  indicationSpeed: 750
-};
-const BUTTONS = {
-  red: {
-    element: document.getElementById("red"),
-    audio: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
-  },
-  blue: {
-    element: document.getElementById("blue"),
-    audio: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
-  },
-  yellow: {
-    element: document.getElementById("yellow"),
-    audio: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
-  },
-  green: {
-    element: document.getElementById("green"),
-    audio: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
-  }
-};
+const BUTTONS = {red, blue, yellow, green};
+const COLORS = Object.keys(BUTTONS);
+const ON_OPACITY = "1";
+const OFF_OPACITY = "0.6";
+const BEEP_INTERVAL = 750;
 let gamePattern = [];
 let playerPattern = [];
 
-randomPattern(3);
-indicatePattern();
-
 // BUTTON FUNCTIONALITY
 for (const color in BUTTONS) {
+  BUTTONS[color].element = document.getElementById(color);
+  BUTTONS[color].audio = new Audio(`./media/${color}.mp3`);
+
   BUTTONS[color].element.addEventListener("click", function() {
     playerPattern.push(color);
     BUTTONS[color].audio.play();
   });
 }
 
+randomPattern(3);
+indicatePattern();
+
 // GENERATE RANDOM PATTERN OF COLORS
 function randomPattern(numOfColors) {
   for (let i = 0; i < numOfColors; i++) {
-    let randomNum = Math.floor(Math.random() * GAME.colors.length);
-    let randomButton = GAME.colors[randomNum];
+    let randomNum = Math.floor(Math.random() * COLORS.length);
+    let randomButton = COLORS[randomNum];
 
     gamePattern.push(randomButton);
   }
@@ -50,20 +34,20 @@ function randomPattern(numOfColors) {
 
 // VISUALIZE PATTERN VIA BUTTON FLASHES AND BEEPS
 function indicatePattern() {
-  let interval = 0;
+  let interval = BEEP_INTERVAL;
 
   gamePattern.forEach(function (color) {
     let element = BUTTONS[color].element;
 
     setTimeout(function () {
-      element.style.opacity = GAME.onOpacity;
+      element.style.opacity = ON_OPACITY;
       BUTTONS[color].audio.play();
       
       BUTTONS[color].audio.addEventListener("ended", function() {
-        element.style.opacity = GAME.offOpacity;
+        element.style.opacity = OFF_OPACITY;
       });
     }, interval);
 
-    interval += GAME.indicationSpeed;
+    interval += BEEP_INTERVAL;
   });
 }
