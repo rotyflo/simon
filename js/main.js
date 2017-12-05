a8c54246b65c582302a55ab9d097167add9f20e8
RT Flores <rotyflo@gmail.com>
2017-12-05 23:18:12 +0900

    Complete all user stories

--- js/main.js
+++ js/main.js
@@ -1,35 +1,100 @@
 "use strict";
 
-const BUTTONS = {red, blue, yellow, green};
+const BUTTONS = { red, blue, yellow, green };
 const COLORS = Object.keys(BUTTONS);
 const ON_OPACITY = "1";
 const OFF_OPACITY = "0.6";
 const BEEP_INTERVAL = 750;
+const NOTIFICATION_TIME = 2000;
+const TURN_INDICATOR = document.getElementById("turn");
 let gamePattern = [];
 let playerPattern = [];
+let strictMode = false;
 
 // BUTTON FUNCTIONALITY
 for (const color in BUTTONS) {
   BUTTONS[color].element = document.getElementById(color);
   BUTTONS[color].audio = new Audio(`./media/${color}.mp3`);
 
-  BUTTONS[color].element.addEventListener("click", function() {
+  BUTTONS[color].element.addEventListener("click", function () {
     playerPattern.push(color);
     BUTTONS[color].audio.play();
+
+    let pp = JSON.stringify(playerPattern);
+    let gp = JSON.stringify(gamePattern);
+
+    // INCORRECT ANSWER
+    if (pp !== gp.slice(0, pp.length - 1) + "]") {
+      let turn = TURN_INDICATOR.innerText;
+      TURN_INDICATOR.innerText = "!!";
+
+      setTimeout(function () {
+        if (strictMode) {
+          restartGame();
+        }
+        else {
+          TURN_INDICATOR.innerText = turn;
+          playerPattern = [];
+
+          indicatePattern();
+        }
+      }, NOTIFICATION_TIME);
+    }
+
+    // CORRECT ANSWER
+    if (pp == gp) {
+      TURN_INDICATOR.innerText = Number(TURN_INDICATOR.innerText) + 1;
+      playerPattern = [];
+
+      if (TURN_INDICATOR.innerText == 20) {
+        TURN_INDICATOR.innerText = "YOU WIN!";
+
+        setTimeout(function () {
+          restartGame();
+        }, NOTIFICATION_TIME);
+      }
+      else {
+        addRandomColorToPattern();
+        indicatePattern();
+      }
+    }
   });
 }
 
-randomPattern(3);
+// STRICT MODE BUTTON
+document.getElementById("strict-mode").addEventListener("click", function () {
+  if (strictMode) {
+    strictMode = false;
+    document.getElementById("strict-mode").innerText = "STRICT MODE DISABLED";
+  } else {
+    strictMode = true;
+    document.getElementById("strict-mode").innerText = "STRICT MODE ENABLED";
+  }
+});
+
+// RESTART BUTTON
+document.getElementById("restart").addEventListener("click", function () {
+  restartGame();
+});
+
+// START GAME
+addRandomColorToPattern();
 indicatePattern();
 
-// GENERATE RANDOM PATTERN OF COLORS
-function randomPattern(numOfColors) {
-  for (let i = 0; i < numOfColors; i++) {
-    let randomNum = Math.floor(Math.random() * COLORS.length);
-    let randomButton = COLORS[randomNum];
+function restartGame() {
+  gamePattern = [];
+  playerPattern = [];
+  TURN_INDICATOR.innerText = 1;
 
-    gamePattern.push(randomButton);
-  }
+  addRandomColorToPattern();
+  indicatePattern();
+}
+
+// GENERATE RANDOM PATTERN OF COLORS
+function addRandomColorToPattern() {
+  let randomNum = Math.floor(Math.random() * COLORS.length);
+
+  gamePattern.push(COLORS[randomNum]);
 }
 
 // VISUALIZE PATTERN VIA BUTTON FLASHES AND BEEPS
@@ -42,8 +107,8 @@ function indicatePattern() {
     setTimeout(function () {
       element.style.opacity = ON_OPACITY;
       BUTTONS[color].audio.play();
-      
-      BUTTONS[color].audio.addEventListener("ended", function() {
+
+      BUTTONS[color].audio.addEventListener("ended", function () {
         element.style.opacity = OFF_OPACITY;
       });
     }, interval);
